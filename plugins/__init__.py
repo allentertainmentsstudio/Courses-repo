89e80240c2eb1ac27ff856556f7ea1b7f3c7013c
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-07 01:10:12 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,7 +11,7 @@ import sys
 async def Start_msg(bot: stark , m: Message):
     await bot.send_photo(
     m.chat.id,
-    photo="https://telegra.ph/file/cef3ef6ee69126c23bfe3.jpg",
+    photo="https://i.ibb.co/s9FfzP3H/7168219724-28038.jpg",
     caption = "**Hi i am All in One Extractor Bot**.\n"
                             "Press **/pw** for **Physics Wallah**..\n\n"
                             "Press **/e1** for **E1 Coaching App**..\n\n"
@@ -41,4 +41,5 @@ async def restart_handler(_, m):
 @stark.on_message(filters.command(["log"]))
 async def log_msg(bot: stark , m: Message):   
     await bot.send_document(m.chat.id, "log.txt")
+
 
